4fb43bcd141a3b1c2d10b95fbf6d2078ade1435d
Tomás Petriche <tockpetri@gmail.com>
2025-06-17 18:03:34 -0300

    feat: adiciona as classes Tarefa e SprintMeta

---
+++ src/modelos/Tarefa.py
@@ -0,0 +1,78 @@
+from datetime import date
+from ItemDeTrabalho import ItemDeTrabalho, StatusItem
+
+class Tarefa(ItemDeTrabalho):
+
+    #Construtor da classe tarefa
+    def __init__(self, id: int, projeto_id: int, nome: str, descricao: str, xp_valor: int, status: StatusItem,
+                 participacao_responsavel_id: int, prazo: date, sprint_meta_id: int):
+        super().__init__(id, projeto_id, nome, descricao, xp_valor, status)
+        self.__participacao_responsavel_id = participacao_responsavel_id
+        self.__prazo = prazo
+        self.__sprint_meta_id = sprint_meta_id
+    
+
+    #Getters:
+
+    #getter do id da participação responsavel pela Tarefa
+    def get_participacao_responsavel_id(self) -> int:
+        return self.__participacao_responsavel_id
+    
+    #getter do prazo da Tarefa
+    def get_prazo(self) -> date:
+        return self.__prazo
+    
+    #getter do id da SprintMeta que a Tarefa faz parte
+    def get_sprint_meta_id(self) -> int:
+        return self.__sprint_meta_id
+    
+
+    #Setters:
+
+    #setter do prazo da Tarefa
+    def set_prazo(self, prazo: date):
+        self.__prazo = prazo
+    
+    #setter do id da participação responsavel pela Tarefa
+    def set_participacao_responsavel_id(self, participacao_responsavel_id: int):
+        self.__participacao_responsavel_id = participacao_responsavel_id
+        
+    #setter do id da SprintMeta que a Tarefa faz parte
+    def set_sprint_meta_id(self, sprint_meta_id: int):
+        self.__sprint_meta_id = sprint_meta_id
+
+    #setter do status da Tarefa. Se a tarefa estiver sido concluida, contribui para projeto com seu xp atribuido
+    def set_status(self, status: StatusItem):
+        if isinstance(status, StatusItem):
+            self.__status = status
+            self.contribuir_para_projeto()
+        else:
+            raise ValueError("status tem que ser uma instancia de StatusItem")  # possivelmente trocar por algum tratamento de erro adequado
+        
+
+    #Representação textual do objeto:
+
+    #retorna dados sobre a tarefa em formato de string
+    def __str__(self):
+        return (f"Tarefa '{self.__nome}':\nStatus: {self.__status.name}\nXP: {self.__xp_valor}\nPrazo: {self.__prazo}")
+    
+
+    #Implementação dos métodos abstratos de ItemDeTrabalho:
+
+    # retorna 1 caso a tarefa esteja concluida e 0 caso nao
+    def calcular_progresso(self):
+        if self.__status == StatusItem.CONCLUIDA:
+            return 1
+        return 0
+    
+    #caso a tarefa esteja concluida, chama adicionar_xp do projeto e da participacao envolvidos
+    def contribuir_para_projeto(self):
+        #if self.__status == StatusItem.CONCLUIDA
+            # chamar metodos de adicionar_xp do projeto e da participacao envolvidos com xp_valor da tarefa como parametro
+        pass
+    
+    #retorna o tipo de ItemDeTrabalho (TAREFA)
+    def obter_tipo_item(self) -> str:
+        return "TAREFA"
+    
+
