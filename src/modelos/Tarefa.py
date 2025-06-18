2a483d8c73e56c0c4fa0d0001a21b083597f91c4
Tomás Petriche <tockpetri@gmail.com>
2025-06-17 23:32:29 -0300

    correção de lógica em Tarefa (remoção de set_status e agora usa corretamente get_status)

--- src/modelos/Tarefa.py
+++ src/modelos/Tarefa.py
@@ -43,33 +43,25 @@ class Tarefa(ItemDeTrabalho):
     def set_sprint_meta_id(self, sprint_meta_id: int):
         self.__sprint_meta_id = sprint_meta_id
 
-    #setter do status da Tarefa. Se a tarefa estiver sido concluida, contribui para projeto com seu xp atribuido
-    def set_status(self, status: StatusItem):
-        if isinstance(status, StatusItem):
-            self.__status = status
-            self.contribuir_para_projeto()
-        else:
-            raise ValueError("status tem que ser uma instancia de StatusItem")  # possivelmente trocar por algum tratamento de erro adequado
-        
 
     #Representação textual do objeto:
 
     #retorna dados sobre a tarefa em formato de string
     def __str__(self):
-        return (f"Tarefa '{self.__nome}':\nStatus: {self.__status.name}\nXP: {self.__xp_valor}\nPrazo: {self.__prazo}")
+        return (f"Tarefa '{self.__nome}':\nStatus: {self.get_status().name}\nXP: {self.__xp_valor}\nPrazo: {self.__prazo}")
     
 
     #Implementação dos métodos abstratos de ItemDeTrabalho:
 
     # retorna 1 caso a tarefa esteja concluida e 0 caso nao
     def calcular_progresso(self):
-        if self.__status == StatusItem.CONCLUIDA:
+        if self.get_status() == StatusItem.CONCLUIDA:
             return 1
         return 0
     
     #caso a tarefa esteja concluida, chama adicionar_xp do projeto e da participacao envolvidos
     def contribuir_para_projeto(self, projeto: Projeto, participacao: Participacao):
-        if self.__status == StatusItem.CONCLUIDA:
+        if self.get_status() == StatusItem.CONCLUIDA:
             if projeto.get_id == self.__projeto_id:
                 if participacao.get_id == self.__participacao_responsavel_id:
                     projeto.adicionar_xp(self.__xp_valor)
