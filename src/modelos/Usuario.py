ed406a13e86f139e1b27f9320636436c9e7aa758
Diogo Barros <diogo2004barros@gmail.com>
2025-06-19 00:11:22 -0300

    feat: Implementa construtor alternativo (criar_com_hash) e remove id da lista de parâmetros do __init__ em Usuario

--- src/modelos/Usuario.py
+++ src/modelos/Usuario.py
@@ -1,14 +1,38 @@
-from Seguranca import gerar_hash_senha, verificar_senha
+from .Seguranca import gerar_hash_senha, verificar_senha
 
 class Usuario:
+    """
+    Representa a entidade Usuario no sistema
+
+    Encapsula os atributos de um usuário e gerencia a segurança da senha
+
+    Um objeto Usuario pode ser inicializado de duas formas diferentes:
+    1. Novo usuário, com senha em texto claro (uso do construtor __init__)
+    2. Usuário existente (lido do BD), com senha_hash conhecida (uso do método de classe criar_com_hash)
+    """
 
     #construtor da classe Usuario
-    def __init__(self, id: int, nome: str, email: str, senha: str):
-        self.__id = id
+    def __init__(self, nome: str, email: str, senha: str):
+        self.__id = None #Inicialização para novos usuários
         self.__nome = nome
         self.__email = email
         self.__senha_hash = gerar_hash_senha(senha)    # a ser armazenada como hash
     
+    @classmethod
+    def criar_com_hash(cls, id_usuario: int, nome: str, email: str, senha_hash: bytes):
+        """
+        Construtor alternativo para Usuario. 
+        
+        Cria uma instância de Usuario a partir de um hash da senha já existente
+        """
+
+        instancia = cls.__new__(cls) #Cria uma nova instância sem chamar o __init__
+        instancia.__id = id_usuario
+        instancia.__nome = nome
+        instancia.__email = email
+        instancia.__senha_hash = senha_hash
+        return instancia
+
     #getters:
 
     #getter de id, retorna id do usuario
@@ -29,7 +53,11 @@ class Usuario:
     
     #setters:
 
-    #setter de nome, altera id para novo_nome
+    #setter de id, altera id para novo_nome
+    def set_id(self, novo_id: int):
+        self.__id = novo_id
+
+    #setter de nome, altera nome para novo_nome
     def set_nome(self, novo_nome: str):
         self.__nome = novo_nome
 
@@ -50,4 +78,10 @@ class Usuario:
             self.__senha_hash = gerar_hash_senha(nova_senha)
             return True
         return False
+
+    def __str__(self) -> str:
+        """
+        Retorna uma representação textual do objeto Usuario
+        """
+        return f"Usuario(id: {self.get_id()}, nome: {self.get_nome()}, email: {self.get_email()})"
 
