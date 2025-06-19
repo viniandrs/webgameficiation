98661979b306cb683c93e2d33730a42c8e0feeb6
Diogo Barros <diogo2004barros@gmail.com>
2025-06-19 03:09:17 -0300

    feat: Implementa os métodos de testes para a classe Usuario

--- tests/test_usuario_modelo.py
+++ tests/test_usuario_modelo.py
@@ -2,6 +2,7 @@
 
 import unittest
 from ..src.modelos.Usuario import Usuario
+from ..src.modelos.Seguranca import gerar_hash_senha
 
 class TestUsuarioModelo(unittest.TestCase):
   """
@@ -12,7 +13,7 @@ class TestUsuarioModelo(unittest.TestCase):
     """
     Método de preparação do ambiente de testes a ser executado antes de cada teste
     """
-    pass
+    self.novo_usuario = Usuario("Diogo", "diogo@gmail.com", "minha_senha")
 
   def tearDown(self):
     """
@@ -24,27 +25,41 @@ class TestUsuarioModelo(unittest.TestCase):
 
   def test_criar_usuario_construtor(self):
     """Verifica se o usuário é criado corretamente"""
-    pass
+    self.assertEqual(self.novo_usuario.get_name(), "Diogo")
+    self.assertEqual(self.novo_usuario.get_email(), "diogo@gmail.com")
+    self.assertNotEqual(self.novo_usuario.get_senha_hash(), "minha_senha")
 
   def test_set_nome(self):
     """Verifica se o nome do usuário pode ser alterado"""
-    pass
+    novo_nome = "Diogo Barros"
+    self.novo_usuario.set_nome(novo_nome)
+    self.assertEqual(self.novo_usuario.get_nome, novo_nome)
 
   def test_verificar_senha_correta(self):
     """Verifica se a checagem da senha retorna True com a senha correta"""
-    pass
+    self.assertTrue(self.novo_usuario.verificar_senha("minha_senha"))
   
   def test_verificar_senha_incorreta(self):
     """Verifica se a checagem da senha retorna False para a senha incorreta"""
-    pass
+    self.assertFalse(self.novo_usuario.verificar_senha("qualquer_senha"))
 
   def test_alterar_senha_sucesso(self):
     """Verifica se a senha é alterada quando a senha atual correta é fornecida"""
-    pass
+    senha_antiga = "minha_senha"
+    hash_senha_antiga = self.novo_usuario.get_senha_hash()
+    nova_senha = "nova_senha"
+    self.assertTrue(self.novo_usuario.alterar_senha(senha_antiga, nova_senha))
+    self.assertTrue(self.novo_usuario.verificar_senha(nova_senha))
+    self.assertNotEqual(self.novo_usuario.get_senha_hash(), hash_senha_antiga)
 
   def test_alterar_senha_falha(self):
     """Verifica se a senha não é alterada quando a senha atual errada é fornecida"""
-    pass
+    senha_errada = "qualquer_senha"
+    hash_senha_certa = self.novo_usuario.get_senha_hash()
+    nova_senha = "nova_senha"
+    self.assertFalse(self.novo_usuario.alterar_senha(senha_errada, nova_senha))
+    self.assertFalse(self.novo_usuario.verificar_senha(senha_errada))
+    self.assertEqual(self.novo_usuario.get_senha_hash, hash_senha_certa)
 
 #Executa os testes quando o arquivo é rodado diretamente
 if __name__ == '__main__': unittest.main()
